41d5b6d2ce4a549fdc4679f2d63fd53a176c725c
Can Şanlı <cansanli.of@gmail.com>
2024-01-06 21:41:30 +0300

    Add project files.

---
+++ EArsivFaturaManagerBot/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿
+namespace EArsivFaturaManagerBot
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tbVKN = new System.Windows.Forms.TextBox();
+            this.cbVknFilterCheck = new System.Windows.Forms.CheckBox();
+            this.bExcelDosyasiSec = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dtpFinishDate = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dtpStartDate = new System.Windows.Forms.DateTimePicker();
+            this.lAccountCounter = new System.Windows.Forms.Label();
+            this.bBotuCalistir = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(396, 450);
+            this.tabControl1.TabIndex = 11;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.LightGray;
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.tbVKN);
+            this.tabPage1.Controls.Add(this.cbVknFilterCheck);
+            this.tabPage1.Controls.Add(this.bExcelDosyasiSec);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.dtpFinishDate);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.dtpStartDate);
+            this.tabPage1.Controls.Add(this.lAccountCounter);
+            this.tabPage1.Controls.Add(this.bBotuCalistir);
+            this.tabPage1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(388, 417);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Fatura Bot";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.MintCream;
+            this.tabPage2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(792, 417);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Xml to Excel Converter";
+            // 
+            // tbVKN
+            // 
+            this.tbVKN.Enabled = false;
+            this.tbVKN.Location = new System.Drawing.Point(9, 310);
+            this.tbVKN.Name = "tbVKN";
+            this.tbVKN.PlaceholderText = "VKN";
+            this.tbVKN.Size = new System.Drawing.Size(249, 27);
+            this.tbVKN.TabIndex = 20;
+            // 
+            // cbVknFilterCheck
+            // 
+            this.cbVknFilterCheck.AutoSize = true;
+            this.cbVknFilterCheck.Location = new System.Drawing.Point(9, 279);
+            this.cbVknFilterCheck.Name = "cbVknFilterCheck";
+            this.cbVknFilterCheck.Size = new System.Drawing.Size(105, 24);
+            this.cbVknFilterCheck.TabIndex = 19;
+            this.cbVknFilterCheck.Text = "VKN filtresi";
+            this.cbVknFilterCheck.UseVisualStyleBackColor = true;
+            // 
+            // bExcelDosyasiSec
+            // 
+            this.bExcelDosyasiSec.Location = new System.Drawing.Point(9, 244);
+            this.bExcelDosyasiSec.Name = "bExcelDosyasiSec";
+            this.bExcelDosyasiSec.Size = new System.Drawing.Size(249, 29);
+            this.bExcelDosyasiSec.TabIndex = 18;
+            this.bExcelDosyasiSec.Text = "Excel Dosyası Seç";
+            this.bExcelDosyasiSec.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(8, 196);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(250, 44);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Giriş bilgilerinin bulunduğu excel dosyasını seçin:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 20);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Bitiş Tarihi:";
+            // 
+            // dtpFinishDate
+            // 
+            this.dtpFinishDate.Location = new System.Drawing.Point(8, 153);
+            this.dtpFinishDate.Name = "dtpFinishDate";
+            this.dtpFinishDate.Size = new System.Drawing.Size(250, 27);
+            this.dtpFinishDate.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 20);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Başlangıç Tarihi:";
+            // 
+            // dtpStartDate
+            // 
+            this.dtpStartDate.Location = new System.Drawing.Point(8, 100);
+            this.dtpStartDate.Name = "dtpStartDate";
+            this.dtpStartDate.Size = new System.Drawing.Size(250, 27);
+            this.dtpStartDate.TabIndex = 13;
+            // 
+            // lAccountCounter
+            // 
+            this.lAccountCounter.AutoSize = true;
+            this.lAccountCounter.Enabled = false;
+            this.lAccountCounter.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lAccountCounter.ForeColor = System.Drawing.Color.OrangeRed;
+            this.lAccountCounter.Location = new System.Drawing.Point(143, 381);
+            this.lAccountCounter.Name = "lAccountCounter";
+            this.lAccountCounter.Size = new System.Drawing.Size(42, 28);
+            this.lAccountCounter.TabIndex = 12;
+            this.lAccountCounter.Text = "0/0";
+            // 
+            // bBotuCalistir
+            // 
+            this.bBotuCalistir.Enabled = false;
+            this.bBotuCalistir.Location = new System.Drawing.Point(8, 380);
+            this.bBotuCalistir.Name = "bBotuCalistir";
+            this.bBotuCalistir.Size = new System.Drawing.Size(129, 29);
+            this.bBotuCalistir.TabIndex = 11;
+            this.bBotuCalistir.Text = "Botu Çalıştır";
+            this.bBotuCalistir.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Palatino Linotype", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(8, 8);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(331, 50);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "E-Arşiv Fatura Bot";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(396, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "E-Arşiv Fatura Manager Bot";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbVKN;
+        private System.Windows.Forms.CheckBox cbVknFilterCheck;
+        private System.Windows.Forms.Button bExcelDosyasiSec;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dtpFinishDate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dtpStartDate;
+        private System.Windows.Forms.Label lAccountCounter;
+        private System.Windows.Forms.Button bBotuCalistir;
+        private System.Windows.Forms.TabPage tabPage2;
+    }
+}
+
---
+++ EArsivFaturaManagerBot/Form1.cs
@@ -0,0 +1,378 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System.Windows.Forms;
+using SeleniumExtras.WaitHelpers;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+namespace EArsivFaturaManagerBot
+{
+    public partial class Form1 : Form
+    {
+        private const string LoginUrl = "https://earsivportal.efatura.gov.tr/intragiris.html";
+        private const string DateFormat = "dd.MM.yyyy";
+        static string ExcelFilePath = string.Empty;
+        static string StartDate = string.Empty;
+        static string FinishDate = string.Empty;
+        static string VKN = string.Empty;
+        public static string[,] UserList;
+
+        private bool IsExcelFileSelected = false;
+        private bool IsStartDateSelected = false;
+        private bool IsFinishDateSelected = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+            FormatDtp(dtpStartDate, DateFormat);
+            FormatDtp(dtpFinishDate, DateFormat);
+
+            
+        }
+
+
+        private void bBotuCalistir_Click(object sender, EventArgs e)
+        {
+            CreateTxtFile();
+            string chromeDriverPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "chromedriver.exe");
+            ChromeOptions chromeOptions = new ChromeOptions();
+
+            ChromeDriverService service = ChromeDriverService.CreateDefaultService(chromeDriverPath);
+            service.HideCommandPromptWindow = true;
+
+
+            StartDate = dtpStartDate.Text;
+            FinishDate = dtpFinishDate.Text;
+
+            Task task = Task.Run(() => GetUserListFromExcel());
+            task.Wait();
+
+            if (UserList != null)
+            {
+               
+                for (int i = 0; i < UserList.GetLength(0); i++)
+                {
+                    lAccountCounter.Text = (i+1) + "/" + UserList.GetLength(0);
+
+                    string username = UserList[i, 0];
+
+                    string password = UserList[i, 1];
+
+
+
+                    // WebDriver'ı başlat
+                    IWebDriver driver = new ChromeDriver(service, chromeOptions);
+                    WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(15));
+                    // E-Fatura Portalı sayfasını aç
+                    driver.Navigate().GoToUrl(LoginUrl);
+
+                    By elementLocator = By.Id("userid");
+                    wait.Until(ExpectedConditions.ElementIsVisible(elementLocator));
+                    // Kullanıcı adı ve şifreyi doldur
+                    driver.FindElement(By.Id("userid")).SendKeys(username);
+                    driver.FindElement(By.Id("password")).SendKeys(password);
+
+                    // Giriş yap butonuna tıkla
+                    driver.FindElement(By.CssSelector("button[onclick='assosLogin()']")).Click();
+                    Thread.Sleep(3000);
+
+
+                    if (driver.Url.Contains("https://earsivportal.efatura.gov.tr/index.jsp"))
+                    {
+                        MessageBox.Show("Giriş Başarılı", "Oturum Açıldı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        System.Threading.Thread.Sleep(2000);
+                        elementLocator = By.Id("gen__1006");
+                        wait.Until(ExpectedConditions.ElementIsVisible(elementLocator));
+
+                        IWebElement selectElement = driver.FindElement(By.Id("gen__1006"));
+                        selectElement.Click();
+                        IList<IWebElement> options = selectElement.FindElements(By.TagName("option"));
+                        options[1].Click();
+                        System.Threading.Thread.Sleep(500);
+                        //fatura bilgi sorgulama sayfa seçimi
+
+                        IWebElement ulElement = driver.FindElement(By.Id("gen__1016-tree_ul"));
+
+                        // soldaki menüden fatura sorgulamaya geldik
+                        IList<IWebElement> liElements = ulElement.FindElements(By.TagName("li")); // Değiştirilecek TagName
+                        System.Threading.Thread.Sleep(500);
+                        // Belirli bir <li> elemanına tıkla (örneğin, 2. <li> elemanına tıkla)
+                        liElements[1].Click();
+                        IList<IWebElement> liElements2 = liElements[1].FindElements(By.TagName("li"));
+                        liElements2[1].Click();
+
+                        //Başlangıç tarihi ayarlandı
+                        elementLocator = By.Id("date-gen__1024");
+                        wait.Until(ExpectedConditions.ElementIsVisible(elementLocator));
+
+                        System.Threading.Thread.Sleep(1000);
+                        var startDatePicker = driver.FindElement(By.Id("date-gen__1024"));
+                        startDatePicker.Clear();
+                        System.Threading.Thread.Sleep(500);
+                        IJavaScriptExecutor jsExecutor = (IJavaScriptExecutor)driver;
+                        jsExecutor.ExecuteScript($"arguments[0].value = '{StartDate}';", startDatePicker);
+                        System.Threading.Thread.Sleep(500);
+                        //Bitiş tarihi ayarlandı
+                        var finishDatePicker = driver.FindElement(By.Id("date-gen__1025"));
+                        finishDatePicker.Clear();
+                        System.Threading.Thread.Sleep(500);
+                        jsExecutor.ExecuteScript($"arguments[0].value = '{FinishDate}';", finishDatePicker);
+                        //Sorgula butonu tıkla
+                        driver.FindElement(By.Id("gen__1026")).Click();
+                        System.Threading.Thread.Sleep(2000);
+
+
+                        //her sayfa için indirme işlemi
+                        elementLocator = By.CssSelector("#gen__1036-div > span:nth-child(5)");
+                        wait.Until(ExpectedConditions.ElementIsVisible(elementLocator));
+                        var tableFooter = driver.FindElement(By.CssSelector("#gen__1036-div > span:nth-child(5)"));
+
+                        int pageCount = tableFooter.Text[1] - (byte)'0'; //tablodaki sayfa sayısını aldım
+
+                        for (int j = 0; j < pageCount; j++)
+                        {
+
+                            Task task2 = Task.Run(() => DownloadPage(driver));
+                            task2.Wait();
+                            Thread.Sleep(1000);
+                            if (j < pageCount - 1)
+                            {
+                                driver.FindElement(By.CssSelector("#gen__1036-div > span.csc-table-paging-btn.csc-table-seek-next")).Click();
+                            }
+                            Thread.Sleep(500);
+
+
+
+
+                        }
+                        Task t = Task.Run(() => { driver.FindElement(By.CssSelector("#gen__1008")).Click(); });
+                        t.Wait();
+                        driver.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show($"Giriş başarısız! Kullanıcı verileri: {username} , {password}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        WriteTxtFile(username + " " + password);
+                        driver.Close();
+                    }
+                }
+            }
+
+        }
+
+        public static void DownloadPage(IWebDriver driver)
+        {
+            //Tablodaki satırları seç
+            IWebElement table = driver.FindElement(By.Id("gen__1036-t"));
+
+            IWebElement tbody = table.FindElement(By.TagName("tbody"));
+
+            // Body içindeki satırları bul
+            IList<IWebElement> rows = tbody.FindElements(By.TagName("tr"));
+            System.Threading.Thread.Sleep(500);
+            // Her satır için işlem yap
+            foreach (var row in rows)
+            {
+
+                var deletedCheck = row;
+                var iptalCheck = row;
+                var vknCheck = row;
+                // Satırdaki hücreleri bul
+                IList<IWebElement> cells = row.FindElements(By.TagName("td"));
+                System.Threading.Thread.Sleep(500);
+                if (cells.Count > 1)
+                {
+
+                    iptalCheck = cells[7].FindElement(By.TagName("span"));
+
+
+                    deletedCheck = cells[6].FindElement(By.TagName("i"));
+
+                    vknCheck = cells[2].FindElement(By.TagName("span"));
+                }
+
+
+
+                foreach (var cell in cells)
+                {
+
+                    if (cell.GetAttribute("class") == "csc-table-select" && iptalCheck.Text == "----------" && deletedCheck.GetAttribute("class") != "fa fa-remove")
+                    {
+                        if (VKN != string.Empty && vknCheck.Text == VKN)
+                        {
+                            cell.FindElement(By.TagName("input")).Click();
+                            System.Threading.Thread.Sleep(500);
+                            driver.FindElement(By.Id("gen__1033")).Click();
+                            System.Threading.Thread.Sleep(750);
+                            cell.FindElement(By.TagName("input")).Click();
+                            System.Threading.Thread.Sleep(500);
+                        }
+                        else if (VKN == string.Empty)
+                        {
+                            cell.FindElement(By.TagName("input")).Click();
+                            System.Threading.Thread.Sleep(500);
+                            driver.FindElement(By.Id("gen__1033")).Click();
+                            System.Threading.Thread.Sleep(3000);
+                            cell.FindElement(By.TagName("input")).Click();
+                            System.Threading.Thread.Sleep(500);
+                        }
+
+                    }
+                    // Burada hücre içeriğiyle yapmak istediğiniz işlemleri gerçekleştirebilirsiniz
+                }
+
+
+            }
+
+
+
+        }
+
+        private void FormatDtp(DateTimePicker dtp, string format)
+        {
+            dtp.Format = DateTimePickerFormat.Custom;
+            dtp.CustomFormat = format;
+
+        }
+        public static void GetUserListFromExcel()
+        {
+
+            // Excel dosyasının yolunu belirtin
+            string excelFilePath = ExcelFilePath;
+
+            if (File.Exists(excelFilePath))
+            {
+                using (var fs = new FileStream(excelFilePath, FileMode.Open, FileAccess.Read))
+                {
+                    IWorkbook workbook = new XSSFWorkbook(fs); // Excel dosyasını aç
+
+                    ISheet sheet = workbook.GetSheetAt(0); // İlk çalışma sayfasını al
+                    UserList = new string[sheet.LastRowNum, 2];
+                    // Satırları dolaş
+                    for (int i = 1; i <= sheet.LastRowNum; i++)
+                    {
+                        IRow row = sheet.GetRow(i);
+                        if (row != null)
+                        {
+                            // Hücreleri dolaş
+                            for (int j = 0; j < 2; j++)
+                            {
+                                ICell cell = row.GetCell(j);
+                                if (cell != null)
+                                {
+                                    UserList[i - 1, j] = cell.ToString();
+
+                                }
+                            }
+
+                        }
+                    }
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Belirtilen excel dosyası bulunamadı", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+        public static void CreateTxtFile()
+        {
+            string dosyaAdi = "BasarisizGirisler.txt";
+
+
+            if (File.Exists(dosyaAdi))
+            {
+                File.WriteAllText(dosyaAdi, string.Empty);
+
+            }
+            else
+            {
+                File.Create(dosyaAdi).Close();
+
+            }
+
+
+
+
+        }
+        public static void WriteTxtFile(string userData)
+        {
+            string dosyaAdi = "BasarisizGirisler.txt";
+
+            File.AppendAllText(dosyaAdi, userData + Environment.NewLine);
+
+
+        }
+
+        private void bExcelDosyasiSec_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dosyaSec = new OpenFileDialog();
+
+            dosyaSec.Filter = "Excel Dosyaları|*.xls;*.xlsx;*.xlsm";
+
+            // Kullanıcıya açılacak olan pencerenin başlığını belirleyebilirsiniz
+            dosyaSec.Title = "Excel Dosyası Seç";
+
+            if (dosyaSec.ShowDialog() == DialogResult.OK)
+            {
+
+                ExcelFilePath = dosyaSec.FileName;
+                lAccountCounter.Text = ExcelFilePath;
+                IsExcelFileSelected = true;
+                CheckBotButtonState();
+            }
+        }
+
+        private void dtpStartDate_ValueChanged(object sender, EventArgs e)
+        {
+            IsStartDateSelected = true;
+            CheckBotButtonState();
+        }
+
+        private void dtpFinishDate_ValueChanged(object sender, EventArgs e)
+        {
+            IsFinishDateSelected = true;
+            CheckBotButtonState();
+        }
+
+        private void CheckBotButtonState()
+        {
+            if (IsStartDateSelected && IsFinishDateSelected && IsExcelFileSelected)
+            {
+                bBotuCalistir.Enabled = true;
+            }
+        }
+
+        private void cbVknFilterCheck_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbVknFilterCheck.Checked)
+            {
+                tbVKN.Enabled = true;
+
+            }
+            else
+            {
+                tbVKN.Enabled = false;
+                tbVKN.Clear();
+
+            }
+
+        }
+
+        private void tbVKN_TextChanged(object sender, EventArgs e)
+        {
+            VKN = tbVKN.Text;
+        }
+    }
+}
